746ffbf23562423a1636c5e8fe451604a8c91c55
pk2202 <77417297+pk2202@users.noreply.github.com>
2021-01-14 06:33:54 +0530

    Created abc.py
    It extension of the file is python

---
+++ abc.py
@@ -0,0 +1,11 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 16:33:24) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39-32/abc.py ===
+enter radius of the circle : 1.1
+area of the circle is: 3.8013271108436504
+>>> 
+==== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39-32/abc.py ===
+enter radius of the circle : 2.2
+area of the circle is: 15.205308443374602
+>>> 
